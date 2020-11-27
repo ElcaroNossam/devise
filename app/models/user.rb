2dc6cd9414ca2fe343c7c919f0c30c44d193a727
Elcaro Nossam <agent_shops@i.ua>
2020-11-27 13:11:34 +0200

    success deploy!

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
         
-         
+         has_many :appointments
          
          validates :number, presence: true, 
                       uniqueness: { case_sensitive: false }, 
