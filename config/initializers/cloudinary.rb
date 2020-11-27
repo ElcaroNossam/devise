8e9903aaefbb7bda048f95e6b8ded844395cdca4
Elcaro Nossam <agent_shops@i.ua>
2020-11-27 12:30:55 +0200

    fix errors

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -2,7 +2,6 @@ Cloudinary.config do |config|
     config.cloud_name = ‘elcaronossam’
     config.api_key = ‘212732786479357’
     config.api_secret = ‘2N7O4mLYfPxojE1wJeERJZeLb1Q’
-    cinfig.cloudinary_url = cloudinary://212732786479357:2N7O4mLYfPxojE1wJeERJZeLb1Q@elcaronossam
     config.secure = true
     config.cdn_subdomain = true
    end
